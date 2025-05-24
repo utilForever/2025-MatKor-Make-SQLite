e0b384b939ae29db3bdea1d13c2cc237a4094ccf
Chris Ohk <utilforever@gmail.com>
2025-05-24 15:31:43 +0900

    chore: Add example codes for renaming temporarily

---
+++ 2 - Code/250508 - Make a Simple SQLite using C++, Week 1/0 - Base code/src/main.cpp
@@ -0,0 +1,13 @@
+#include <cstring>
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+
+    // TODO: Solve Assignment 1 and 2
+
+    return 0;
+}
